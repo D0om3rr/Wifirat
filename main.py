ae38957af4e3ddba64ee3bc227d8a8c5f9e76d5d
Doom33r <doomer17tre@proton.me>
2026-01-03 13:35:00 +0100

    Update Main.py
    Making a cleaner Main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
 from interfaces.interface_startup import interface_startup
+from recon.scan import scanner
+from interfaces.interface_manager import NetworkInterfaceManager 
 
 def check_root():
     return os.getuid() == 0
-
 
 def main():
     print("[+] WIFIRAT")
@@ -17,8 +18,9 @@ def main():
     print("[+] Running script with root privileges")
     print("[+] Scanning and selecting interfaces...")
     
-    
     interface_startup()
 
+    if interface_startup:
+        pass
 if __name__ == '__main__':
     main()
